21ffb6da2735f9c53564a49195b564bdd96755c6
chokoon4686@gmail.com <chokoon4686@gmail.com>
2025-05-10 22:01:30 +0700

    ADD streamlit service

--- work/deploy.py
+++ work/deploy.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
         name="airquality_deployment",
         parameters={},
         work_pool_name="default-agent-pool",
-        cron="*/40 * * * *",  
+        cron="*/2 * * * *",  
     )
