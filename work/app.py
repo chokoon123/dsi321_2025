610009b08d3879b542106fb30704ed938e4d3dc0
chokoon4686@gmail.com <chokoon4686@gmail.com>
2025-05-13 09:49:53 +0700

    Add session streamlit

--- work/app.py
+++ work/app.py
@@ -1,2 +1,29 @@
+import os
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 import streamlit as st
-import pandas as pd 
+import pyarrow.parquet as pq
+import s3fs
+import time
+from zoneinfo import ZoneInfo
+from datetime import timedelta, datetime
+
+lakefs_endpoint = os.getenv("LAKEFS_ENDPOINT", "http://lakefs-dev:8000")
+ACCESS_KEY = os.getenv("LAKEFS_ACCESS_KEY")
+SECRET_KEY = os.getenv("LAKEFS_SECRET_KEY")
+
+fs = s3fs.S3FileSystem(
+    key=ACCESS_KEY,
+    secret=SECRET_KEY,
+    client_kwargs={'endpoint_url': lakefs_endpoint}
+)
+
+if 'last_load_time' not in st.session_state:
+    st.session_state.last_load_time = time.time()
+
+if time.time() - st.session_state.last_load_time > 2400:
+    st.cache_data.clear()
+    st.session_state.last_load_time = time.time()
+    st.experimental_rerun()
